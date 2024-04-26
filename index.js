0d1469094cdc4eed77416bb3fe0eb5478557c28a
KASHIF-SHOUKAT-SE <149269224+KASHIF-SHOUKAT-SE@users.noreply.github.com>
2024-04-26 13:19:54 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+// let sum = 0;
+// let n = prompt("enter your number");
+// n = Number.parseInt(n)
+// for(let i=0; i<n; i++){
+//     sum += (i+1);
+// }
+// console.log("sum of first" + n + "natural number is " + sum)
+
+// for in loop
+// let obj ={
+//     kashif:67,
+//     waleed: 87,
+//     naeem: 69,
+//     majid:89,
+// }
+// for (let a in obj){
+//     console.log("marks of " + a + " are " +obj[a]);
+// }
+
+// for (let b of "kashif"){
+//     console.log(b);
+// }
+
+
+// let n = prompt("enter your number ")
+// n = Number.parseInt(n)
+// let i=0;
+// while(i<n){
+//     console.log(i)
+//     i++;
+// }
+
+let n = prompt("enter your number ")
+n = Number.parseInt(n)
+let i=0;
+do{
+    console.log(i)
+    i++;
+}while(i<n)
+
+
+
+
+
